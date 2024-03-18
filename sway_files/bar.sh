f355b97982156127fe1d8cf1113829e4b2129547
Charles <chaseuelt@gmail.com>
2024-03-18 16:46:51 -0500

    minor edits

--- sway_files/bar.sh
+++ sway_files/bar.sh
@@ -2,9 +2,16 @@ while true; do
 	# $(cmd) stores the output of cmd
 	date=$(date +'%A, %b %d')
 	time=$(date +'%I:%M %p')
+	level=$(cat /sys/class/power_supply/BAT1/capacity)
+	status=$(cat /sys/class/power_supply/BAT1/status)
 
+	if [ $status == "Charging" ]; then
+		charge="🔋 $level%"
+	else
+		charge="⚡ $level%"
+	fi
 	# => Friday, Mar 05 | 03:47 PM
 	weather=$(cat /tmp/weather)
-	echo "$weather | $date | $time"
+	echo "$weather  | $date  |  $time  | $charge"
 	sleep 1
 done
