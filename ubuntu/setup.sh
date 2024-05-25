b466644a5f32135a15560429fdab7894afde2ab5
Charles Ueltschey <chaseuelt@gmail.com>
2024-05-25 18:12:33 -0500

    organize files

---
+++ ubuntu/setup.sh
@@ -0,0 +1,28 @@
+sudo apt update && upgrade -y
+
+# Setup foot sway and rofi
+mkdir -pv ~/.config/foot ~/.config/sway ~/.config/rofi/
+cp ./foot.ini ~/.config/foot/foot.ini
+sudo cp ./sway_config /etc/sway/config
+cp ./rofi-config.rasi ~/.config/rofi/config.rasi
+
+swaymsg reload
+
+apt install swaybg swayidle swaylock waybar
+
+# Install all the necessary tools
+./get_gh.sh
+apt install make gcc snap cmake binutils bison gawk texinfo
+
+# configure nvim
+snap install nvim --classic
+git clone https://github.com/LazyVim/starter ~/.config/nvim
+
+# install nice things
+sudo apt install chromium-browser qutebrowser
+
+gh auth login
+gh extension install https://github.com/sarumaj/gh-gr
+gh extension install wuwe1/gh-ls
+gh extension install Link-/gh-token
+gh extension install gennaro-tedesco/gh-s
