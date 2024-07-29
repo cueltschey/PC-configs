7084f9efa08bb5d13c301a0752553af274e825ae
Charles Ueltschey <chaseuelt@gmail.com>
2024-07-29 13:11:44 -0600

    fixed issues with setup

--- setup.sh
+++ setup.sh
@@ -8,7 +8,7 @@ cp ./rofi-config.rasi ~/.config/rofi/config.rasi
 
 swaymsg reload
 
-apt install swaybg swayidle swaylock waybar
+apt install swaybg swayidle swaylock waybar grimshot rofi
 
 # Install all the necessary tools
 ./get_gh.sh
@@ -19,7 +19,7 @@ snap install nvim --classic
 git clone https://github.com/LazyVim/starter ~/.config/nvim
 
 # install nice things
-sudo apt install chromium-browser qutebrowser
+sudo apt install chromium-browser qutebrowser fzf
 
 gh auth login
 gh extension install https://github.com/sarumaj/gh-gr
