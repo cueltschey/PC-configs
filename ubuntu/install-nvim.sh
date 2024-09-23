a9af0428f1559be7eded509d011eb590fae09206
Joshua Moore <joshuamoore314@icloud.com>
2024-09-23 11:19:52 -0500

    fix nvim install

--- ubuntu/install-nvim.sh
+++ ubuntu/install-nvim.sh
@@ -2,5 +2,6 @@
 curl -LO https://github.com/neovim/neovim/releases/latest/download/nvim-linux64.tar.gz
 sudo rm -rf /opt/nvim
 sudo tar -C /opt -xzf nvim-linux64.tar.gz
-git clone https://github.com/LazyVim/starter ~/.config/nvim
-echo "export PATH=$PATH:/opt/nvim-linux64/bin" >>~/.bashrc
+git clone https://github.com/cueltschey/neovim-config ~/.config/nvim
+echo "export PATH=$PATH:/opt/nvim-linux64/bin" >> ~/.bashrc
+source ~/.bashrc
