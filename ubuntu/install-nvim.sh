7cb7fade3a7ef4f686d667f118f80eb792b3d131
Charles Ueltschey <chaseuelt@gmail.com>
2024-09-30 13:04:45 -0400

    add timezone update script

--- ubuntu/install-nvim.sh
+++ ubuntu/install-nvim.sh
@@ -4,5 +4,5 @@ curl -LO https://github.com/neovim/neovim/releases/latest/download/nvim-linux64.
 sudo rm -rf /opt/nvim
 sudo tar -C /opt -xzf nvim-linux64.tar.gz
 git clone https://github.com/cueltschey/neovim-config ~/.config/nvim
-echo "export PATH=$PATH:/opt/nvim-linux64/bin" >> ~/.bashrc
+echo "export PATH=$PATH:/opt/nvim-linux64/bin" >>~/.bashrc
 source ~/.bashrc
